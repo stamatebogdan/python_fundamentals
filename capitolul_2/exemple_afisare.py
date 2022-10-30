13857daca27228f3d12b306f01da68347bbfd398
Bogdan Stamate <stamatebogdanvalentin@yahoo.com>
2022-10-30 09:14:31 +0200

    test1

---
+++ capitolul_2/exemple_afisare.py
@@ -0,0 +1,37 @@
+mesaj = "CURSUL DE PYTHON"\
+    " SE DESFASOARE" \
+    " IN FIECARE SAPTAMANA" \
+    " LUNI SEARA"
+
+#print(mesaj)
+
+mesaj = """SALUTARE. ASTAZI DISCUTAM 
+        DESPRE SIRURI DE CARACTERE 
+        IN LIMBAJUL PYTHON"""
+#print(mesaj)
+
+mesaj = "Salut. Cursul incepe. \nVa rog sa scoateti o foaie de hartie. \nDam un test."
+#print(mesaj)
+
+#print("TELECOM ACADEMY \t CURSURI DE PROGRAMARE");
+#print("WE LOVE PYTHON \u2763")
+
+#print("MERGEM SA MANCAM LA KFC's");
+#print('Telecom Academy \'s Courses')
+#print("Numele meu este Tom \"TopGun\" Cruise")
+#print(r"Avem de cumparat portocale\tomate\nuci\mere")
+
+#print("C:\\users\\hcalin\\python\\my_file.txt")
+
+#print("Hello" + " World")
+#print("Eu am " + str(17) + " ani")
+
+#print("Eu am {0} ani. Tu ai {1} ani".format(17,27))
+
+#varsta_mea=17
+#varsta_ta=27
+#print(f"Eu am {varsta_mea} ani. Tu ai {varsta_ta} ani.")
+
+
+
+
