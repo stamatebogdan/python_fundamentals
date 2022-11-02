a5e6f57b0858fa75a6f150407a866fc57dc531ca
Bogdan Stamate <stamatebogdanvalentin@yahoo.com>
2022-11-02 18:39:08 +0200

    Capitolul3

---
+++ capitolul_3/exemple_extra/extra_strings.py
@@ -0,0 +1,23 @@
+exemplu = "Testam slicing"
+print(exemplu[0:6])
+
+print(exemplu[7:])
+
+print(exemplu[:-1])
+
+print(exemplu[:])
+
+exemplu2 = "Testam Repetitia"
+
+print(exemplu2 * 2)
+
+print(exemplu2 * 20)
+
+print(exemplu2.startswith('T'))
+print(exemplu2.startswith('a'))
+
+print(exemplu2.endswith('a'))
+print(exemplu2.endswith('itia'))
+
+print(exemplu2.endswith("Testam Repetitia"))
+print(exemplu2.startswith("Testam Repetitia"))
---
+++ capitolul_3/exemple_liste.py
@@ -0,0 +1,46 @@
+# my_list= [12, "sir de caractere", True, False, 10.7, -123, 10.5, -2.4, 10, 2, 10, 7 ,10]
+#
+# print(len(my_list))
+# print(my_list[5])
+# print(my_list[5:])
+# print(my_list[0:10:2])
+#
+# my_list.append("new value")
+# my_list.append(10)
+# my_list.append(100)
+# print(my_list)
+# my_list.insert(0,"PRIORITY ELEMENT")
+# print(my_list)
+#
+# print(my_list+[1,2,3])
+
+# empty_list=list()
+# empty_list.extend([10,20,30, 10, 10, 20, 30, 30])
+# print(empty_list)
+#
+# print(empty_list.pop(2))
+# empty_list.remove(10)
+# print(empty_list)
+# print(min(empty_list))
+# print(max(empty_list))
+#
+# empty_list.sort()
+# print(empty_list)
+# cuvinte = ["banana", "porumb", "televizor", "medicamente"]
+# cuvinte.sort(reverse=True)
+# print(cuvinte)
+# print(list("sir de caractere"))
+# print("sir de caractere".split())
+
+lista_mea=[[1,2,3], [4,5,6], [7,8,9]]
+cuvinte = ["banana", "porumb", "televizor", "medicamente"]
+
+# for cuv in cuvinte:
+#     print(cuv.upper(), end=" ")
+#
+# for i in range(len(cuvinte)):
+#     print(cuvinte[i], end=" ")
+
+print(lista_mea[0][1])
+print(lista_mea[2][2])
+
